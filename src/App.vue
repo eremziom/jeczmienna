8c4cb4d8b260d4095bb749a68c632230e6fb38c1
Maciej Kryczka <maciej.kryczka@toya.com.pl>
2021-12-29 13:27:40 +0100

    start project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container">
+  <div class="imgBaner">
+  </div>
+  <h2 style="margin: 0; opacity: 1; color: black; z-index: 10;">Jęczmienna 33</h2>
+</div>
+  <!-- <img alt="Vue logo" class="imgBaner" src="./assets/nove-zdrovie.jpeg"> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
@@ -21,6 +24,23 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.container {
+  position: relative;
+  z-index: 1;
+}
+.imgBaner {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  opacity: 0.3;
+  filter: invert(100%);
+  background-size: cover;
+  background-image: url('./assets/nove-zdrovie.jpeg')
+}
+body {
+  margin: 0;
 }
 </style>
